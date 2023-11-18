f8329c57c69e620dab974b565cc0d26ca1d09d40
kuo <charliech17@yahoo.com.tw>
2023-11-18 15:35:19 +0800

    優化解答

--- 202306/03.py
+++ 202306/03.py
@@ -1,63 +1,43 @@
-"""
-    T 指令的格式為 Txx，其中 xx 是兩位數的整數（10~99），代表指針從當前位置移動到 xx 所指示的位置。
-    Ex:
-        T10T15T23T23T22T22T44
-            5 + 8 + 0 + 1 + 0 + 22 = 36
-        T10L2T15T22L2T15ET23ET44
-            10 -> 15: 5
-            L中
-                start: [15]
-                l_sum = [(7+7*2+1)*2] + 8 
-"""
-
 def main():
     all_instructions = input() # "T10T15L2L3T10T12ET13E"
     i = 0
     max_index = len(all_instructions) -1
-    now_info = {"now_pos":0,"total":0}
+    now_info = {"now_pos":"","total":0}
     loop_info = [] # [{start: int,last_pos: int,sum: int,times: int}]
     while i <= max_index:
+        now_char = all_instructions[i]
         # !! 遇到字母L
-        if all_instructions[i] == "L":
+        if now_char == "L":
             loop_times = int(all_instructions[i+1])
-            loop_info.append({"start": 0,"last_pos":0,"sum":0,"times": loop_times})
+            loop_info.append({"start": "","last_pos":"","sum":0,"times": loop_times})
             i+=2 # 例如L9T12 -> 從L9到T
-
         # !! 如果在迴圈內
         elif loop_info:
-            if all_instructions[i] == "E":
+            if now_char == "E":
                 last_loop = loop_info.pop()
                 loop_start_end_diff = abs(last_loop["start"] - last_loop["last_pos"])
+                
                 # 取得abs(進入前最後位置 - 進入的第一位置) -> 要分別有否未結束的迴圈情境
                 enter_start_diff = None
-                if loop_info:
+                if loop_info: # 在迴圈中
                     second_last_loop = loop_info[-1]
-                    enter_start_diff = abs(last_loop["start"] - second_last_loop["last_pos"]) if second_last_loop["start"] else 0
+                    enter_start_diff = get_enter_start_diff1(last_loop,second_last_loop)
                     second_last_loop["last_pos"] = last_loop["last_pos"]
                     if not second_last_loop["start"]:
                         second_last_loop["start"] = last_loop["start"]
-                else:
-                    enter_start_diff = abs(now_info["now_pos"] - last_loop["start"]) if now_info["now_pos"] else 0
-                    now_info["now_pos"] = last_loop["last_pos"]
-
-                # "頭到尾差距 ＊ 次數 - 1" + "該loop總和 * loop次數 " + "進入前最後位置 - 進入的第一位置"
-                loop_sum = ( 
-                    (loop_start_end_diff * (last_loop["times"] - 1)) + 
-                    (last_loop["sum"] * last_loop["times"]) +
-                    enter_start_diff
-                )
-
-                if loop_info:
-                    second_last_loop = loop_info[-1]
+                    loop_sum = get_loop_sum(loop_start_end_diff,last_loop,enter_start_diff)
                     second_last_loop["sum"] += loop_sum
-                else:
+                else:         # 不在迴圈中
+                    enter_start_diff = get_enter_start_diff2(now_info,last_loop)
+                    now_info["now_pos"] = last_loop["last_pos"]
+                    loop_sum = get_loop_sum(loop_start_end_diff,last_loop,enter_start_diff)
                     now_info["total"] += loop_sum
-                # 更新i
+
                 i+=1
-            elif all_instructions[i] == "T":
+            elif now_char == "T":
                 num = get_num(all_instructions,i)
                 last_loop = loop_info[-1]
-                if last_loop["last_pos"] == 0:
+                if not last_loop["last_pos"]:
                     last_loop["last_pos"] = num
                     last_loop["start"] = num
                 else:
@@ -70,7 +50,7 @@ def main():
         elif all_instructions[i] == "T":
             num = get_num(all_instructions,i)
 
-            if now_info["now_pos"] == 0:
+            if not now_info["now_pos"]:
                 now_info["now_pos"] = num
             else:
                 diff = abs(now_info["now_pos"] - num)
@@ -79,7 +59,38 @@ def main():
             i+=3
     print(now_info["total"])
 
+
 def get_num(all_instructions,i):
     return int(all_instructions[i+1] + all_instructions[i+2])
+
+
+def get_enter_start_diff1(last_loop,second_last_loop):
+    enter_start_diff = (
+        abs( last_loop["start"] - second_last_loop["last_pos"])
+        if second_last_loop["start"] 
+        else 
+        0
+    )
+    return enter_start_diff
+
+
+def get_enter_start_diff2(now_info,last_loop):
+    enter_start_diff =( 
+        abs(now_info["now_pos"] - last_loop["start"]) 
+        if now_info["now_pos"] 
+        else 0
+    )
+    return enter_start_diff
+
+
+def get_loop_sum(loop_start_end_diff,last_loop,enter_start_diff):
+    # "頭到尾差距 ＊ 次數 - 1" + "該loop總和 * loop次數 " + "進入前最後位置 - 進入的第一位置"
+    loop_sum = ( 
+        (loop_start_end_diff * (last_loop["times"] - 1)) + 
+        (last_loop["sum"] * last_loop["times"]) +
+        enter_start_diff
+    )
+    return loop_sum
+
 
 main()
