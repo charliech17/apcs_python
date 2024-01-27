1a5c8d5d6e18bf39a1f712e86fc7bba74f49b565
Josh <kcchang1994@gmail.com>
2024-01-27 16:52:24 +0800

    another way of solve 202401_02

--- 202401/02.py
+++ 202401/02.py
@@ -64,4 +64,31 @@ def move(pos_code,bee_map,x,y,record,diff_char):
         diff_char.add(prev_word)
         return ori_x, ori_y
 
+def main():
+    m,n,k = [int(i) for i in input().split(" ")]
+    bee_map, record = [], ""
+
+    # map
+    for _ in range(m):
+        each_row = [i for i in input()]
+        bee_map.append(each_row)
+
+    # move_direct
+    move_direct = [int(i) for i in input().split(" ")]
+    
+    # start moving
+    x,y = 0,len(bee_map) - 1
+    dx,dy = (0,1,1,0,-1,-1), (-1,0,1,1,0,-1)
+    for pos_code in move_direct:
+        nx = x + dx[pos_code]
+        ny = y + dy[pos_code]
+
+        if 0<= ny < m and 0<= nx< n:
+            x,y = nx,ny
+        record += bee_map[y][x]
+
+    # print result
+    print(record)
+    print(len(set(record)))
+
 main()
