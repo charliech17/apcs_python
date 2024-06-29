664b4d4da4f5281a8cf8b09230943aefb718ff4d
Josh <kcchang1994@gmail.com>
2024-06-29 15:28:57 +0800

    add some comment and little refactor to 1.py

--- 202406/1.py
+++ 202406/1.py
@@ -4,20 +4,20 @@ max_continue = 1
 final_index = n-1
 
 for b_index,each_b_h in enumerate(h_list):
-
+    # init some values
     temp_max_continue = 1
     now_b_h = each_b_h
     
-    if b_index == final_index:
-        # final index no need to calculate
-        break
-    for j in range(b_index+1,final_index+1):
-        if now_b_h > h_list[j]:
-            temp_max_continue += 1
-            now_b_h = h_list[j]
-        else:
-            break
+    # will calculate if not final index
+    if b_index != final_index:
+        for j in range(b_index+1,final_index+1):
+            if now_b_h > h_list[j]:
+                temp_max_continue += 1
+                now_b_h = h_list[j]
+            else:
+                break
 
+    # if temp > final, set final to temp
     if temp_max_continue > max_continue:
         max_continue = temp_max_continue
 
